6fdc1f0e2791d9fc3960ac4aa4e1af94f7b75d45
MatteoMara <matte.mara@gmail.com>
2018-09-04 09:07:41 +0200

    Adding entities

---
+++ src/main/java/it/mediacontents/backoffice/entities/Tool.java
@@ -0,0 +1,52 @@
+package it.mediacontents.backoffice.entities;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Tool {
+
+    @Id
+    private Long id;
+
+    @ManyToOne
+    private Brand brand;
+
+    private String name;
+
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Brand getBrand() {
+        return brand;
+    }
+
+    public void setBrand(Brand brand) {
+        this.brand = brand;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
